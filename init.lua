06066056b6d31c9f7b5385ae02c8406665497096
root <root@2raj.localdomain>
2025-07-17 12:51:09 +0000

    setup autopairs

--- init.lua
+++ init.lua
@@ -262,6 +262,30 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true,
+    ops = {
+      enabled = function(bufnr) return true end, -- control if auto-pairs should be enabled when attaching to a buffer
+      disable_filetype = { "TelescopePrompt", "spectre_panel", "snacks_picker_input" },
+      disable_in_macro = true, -- disable when recording or executing a macro
+      disable_in_visualblock = false, -- disable when insert after visual block mode
+      disable_in_replace_mode = true,
+      ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
+      enable_moveright = true,
+      enable_afterquote = true, -- add bracket pairs after quote
+      enable_check_bracket_line = true, --- check bracket in same line
+      enable_bracket_in_quote = true, --
+      enable_abbr = false, -- trigger abbreviation
+      break_undo = true, -- switch for basic rule break undo sequence
+      check_ts = true,
+      map_cr = true,
+      map_bs = true, -- map the <BS> key
+      map_c_h = false, -- Map the <C-h> key to delete a pair
+      map_c_w = false, -- map <c-w> to delete a pair if possible
+    },
+  },
+  {
     'rmagatti/auto-session',
     lazy = false,
     keys = {
